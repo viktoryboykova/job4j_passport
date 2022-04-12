650f807cd436fb96eff1eb6cc0bb3b011821e0ec
viktoryboykova <viktoryboykova@list.ru>
2022-04-12 12:50:37 +0300

    create job4j_passport

---
+++ src/main/java/ru/job4j/passport/Job4jPassportApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j.passport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job4jPassportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Job4jPassportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/job4j/passport/Job4jPassportApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.job4j.passport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Job4jPassportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
